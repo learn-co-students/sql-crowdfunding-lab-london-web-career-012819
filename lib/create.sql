d16eb3a5e77248b03c3ca1ca7b14b9158b0cc5dd
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-02-07 15:37:18 +0000

    DOne

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,6 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal TEXT,
+                        Start_Date INTEGER, End_Date INTEGER);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, User_Id INTEGER, Project_Id INTEGER);
